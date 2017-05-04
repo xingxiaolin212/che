1e3f4b2b5c2326805e5bdd63fcadcfdb78b15172
Angel Misevski <amisevsk@redhat.com>
2017-05-04 16:48:03 -0400

    Add property to control manual workspace dir creation in OpenShift
    Add property 'che.openshift.precreate.workspace.dirs'. If property is
    true, OpenShiftConnector will run a pod before launching workspaces
    to create a subpath in the workspace's persistent volume with correct
    permissions. If the property is false, this step is skipped.
    
    This is necessary as in older versions of OpenShift/Kubernetes, subpaths
    created as part of a volume mount are created with root permissions, and
    so cannot be modified by workspace pods. More recent versions fix this,
    creating subpath volumes with correct permissions, making the step above
    unnecessary.
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- plugins/plugin-docker/che-plugin-openshift-client/src/main/java/org/eclipse/che/plugin/openshift/client/OpenShiftConnector.java
+++ plugins/plugin-docker/che-plugin-openshift-client/src/main/java/org/eclipse/che/plugin/openshift/client/OpenShiftConnector.java
@@ -193,6 +193,7 @@ public class OpenShiftConnector extends DockerConnector {
     private final String          cheWorkspaceMemoryLimit;
     private final String          cheWorkspaceMemoryRequest;
     private final boolean         secureRoutes;
+    private final boolean         createWorkspaceDirs;
     private final OpenShiftPvcHelper openShiftPvcHelper;
 
     @Inject
@@ -212,7 +213,8 @@ public class OpenShiftConnector extends DockerConnector {
                               @Named("che.workspace.projects.storage") String cheWorkspaceProjectsStorage,
                               @Nullable @Named("che.openshift.workspace.memory.request") String cheWorkspaceMemoryRequest,
                               @Nullable @Named("che.openshift.workspace.memory.override") String cheWorkspaceMemoryLimit,
-                              @Named("che.openshift.secure.routes") boolean secureRoutes) {
+                              @Named("che.openshift.secure.routes") boolean secureRoutes,
+                              @Named("che.openshift.precreate.workspace.dirs") boolean createWorkspaceDirs) {
 
         super(connectorConfiguration, connectionFactory, authResolver, dockerApiVersionPathPrefixProvider);
         this.cheServerExternalAddress = cheServerExternalAddress;
@@ -226,6 +228,7 @@ public class OpenShiftConnector extends DockerConnector {
         this.cheWorkspaceMemoryRequest = cheWorkspaceMemoryRequest;
         this.cheWorkspaceMemoryLimit = cheWorkspaceMemoryLimit;
         this.secureRoutes = secureRoutes;
+        this.createWorkspaceDirs = createWorkspaceDirs;
         this.openShiftPvcHelper = openShiftPvcHelper;
         eventService.subscribe(new EventSubscriber<ServerIdleEvent>() {
 
@@ -1142,7 +1145,9 @@ public class OpenShiftConnector extends DockerConnector {
 
         LOG.info("Adding container {} to OpenShift deployment {}", sanitizedContainerName, deploymentName);
 
-        createWorkspaceDir(volumes);
+        if (createWorkspaceDirs) {
+            createWorkspaceDir(volumes);
+        }
 
         Container container = new ContainerBuilder()
                                     .withName(sanitizedContainerName)
