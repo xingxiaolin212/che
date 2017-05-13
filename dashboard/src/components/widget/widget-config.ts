0fea3b923a1e0f3ffebbe8ac771fed4a8da14fd8
Oleksii Kurinnyi <okurinnyi@codenvy.com>
2017-05-13 18:05:33 +0300

    CHE-2971: add toggle widgets
    added directives:
    - che-toggle-joined
    - che-toggle-joined-button
    - che-toggle-single-button
    
    Signed-off-by: Oleksii Kurinnyi <okurinnyi@codenvy.com>

--- dashboard/src/components/widget/widget-config.ts
+++ dashboard/src/components/widget/widget-config.ts
@@ -70,6 +70,9 @@ import {ChePopup} from './popup/che-popup.directive';
 import {CheModalPopup} from './popup/che-modal-popup.directive';
 import {CheShowArea} from './show-area/che-show-area.directive';
 import {DemoSourceRender} from './html-source/demo-source-render.directive';
+import {ToggleSingleButton} from './toggle-button/toggle-single-button.directive';
+import {CheToggleJoinedButton} from './toggle-button/che-toggle-joined-button.directive';
+import {CheToggleJoined} from './toggle-button/che-toggle-joined.directive';
 
 
 export class WidgetConfig {
@@ -148,6 +151,9 @@ export class WidgetConfig {
       .controller('CheToggleController', CheToggleController)
       .directive('cheToggleButton', CheToggleButton)
       .directive('cheToggle', CheToggle)
+      .directive('cheToggleJoined', CheToggleJoined)
+      .directive('cheToggleJoinedButton', CheToggleJoinedButton)
+      .directive('toggleSingleButton', ToggleSingleButton)
 
       .directive('cheToolbar', CheToolbar)
       // notifications
